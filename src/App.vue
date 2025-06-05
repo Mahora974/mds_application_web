aa260d243dbb25947dda13ead4b9d660405290c7
Mahora Grolleau <hiphopfraise@gmail.com>
2025-06-05 14:04:07 +0200

    Fix : more direct call
    \+ Resize logo link

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { ref, computed } from 'vue'
-import Home from './pages/Home.vue'
-import Add from './pages/Add.vue'
 
 </script>
 
 <template>
 
   <header class="bg-[#157A6E] px-5">
-    <router-link :to="{name: 'home'}"><img alt="Vue logo" class="logo" src="./assets/logo.svg" width="25" height="25" /></router-link>
+    <router-link :to="{name: 'home'}" class="block size-fit"><img alt="Vue logo" class="logo my-2" src="./assets/logo.svg" width="25" height="25" /></router-link>
   </header>
   <router-view v-slot="{ Component, route }">
     <transition name="fade">
